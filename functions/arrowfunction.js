68c102a3f0b15e91a06c6155e0cff16172b6a3f2
Kimberlly Amaral <kimberllys.amaral@gmail.com>
2021-10-17 19:55:15 -0300

    add functionSetInterval

--- functions/arrowfunction.js
+++ functions/arrowfunction.js
@@ -8,3 +8,13 @@ dobro = (a) => {
 
 dobro = a => 2 * a // return implícito
 console.log(dobro(Math.PI))
+
+// outro exemplo de função comum
+let ola = function () {
+    return 'Olá'
+}
+
+// a mesma função refatorada com arrow function 
+ola = () => 'Olá'
+ola = _ => 'Olá' //possui um parametro
+console.log(ola())
