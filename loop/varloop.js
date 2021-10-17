2f3cf0bbd97196feaa96ec795784e8909c37c6e6
Kimberlly Amaral <kimberllys.amaral@gmail.com>
2021-10-17 02:22:42 -0300

    add var loop

---
+++ loop/varloop.js
@@ -0,0 +1,8 @@
+for (var i = 0; i<10; i++) {
+    console.log(i)
+}
+// Neste caso, se o console.log estivesse fora do bloco, var ainda estaria visível fora do escopo.
+
+//console.lo('i =', i)
+
+// se trocarmos a palavra var por let, o valor de i não estará mais visivel. Pois let não é visível fora do escopo.
